bfef138af0275eab5c7791a41b2885ecb8ae14c9
Samruddhi21-coder <samruddhi.shrawagi@gmail.com>
2025-04-19 20:32:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+let express = require('express');
+let cors = require("cors");
+let {MongoClient} = require("mongodb");
+let app = express();
+app.use(cors()),
+app.use(express.json());
+
+// const url = "mongodb://0.0.0.0:27017";
+
+let url ="mongodb+srv://samruddhishrawagi:932HU5DajnvD6lKt@cluster0.umpkumz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+app.post("/add",(request,response)=>{
+    let client = new MongoClient(url);
+    client.connect();
+
+    let db = client.db("mern");
+    let collec = db.collection("events");
+    let obj = {
+        name : request.body.name,
+        event : request.body.event,
+        time : request.body.time,
+        phone : request.body.phone
+    }
+    collec.insertOne(obj)
+    .then((result)=>response.send(result))
+    .catch((error)=>response.send(error));
+})
+
+
+app.get("/get",(request,response)=>{
+    let client= new MongoClient(url);
+    client.connect();
+
+    let db = client.db("mern");
+    let collec = db.collection("events");
+    collec.find().toArray()
+    .then((result)=>response.send(result))
+    .catch((error)=>response.send(error));
+})
+
+
+
+
+app.listen(9000,()=>{console.log("Express is ready")});
+
+
+
